9d465edd290dc9a39c5bbcf0770e1562cf592409
Ryan Mileris <ryanmileris@yahoo.com>
2019-04-09 21:12:09 -0600

    Finished implementing the simple markov calculator

---
+++ Markov Calculator/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_Calculator
+{
+    public class Calculator
+    {
+        double[,] transition;
+        double[,] init_x;
+        int col_dim;
+        int row_dim;
+
+        int result_row_dim;
+        int result_col_dim;
+
+        public Calculator(double[,] transition, double[,] prev_x)
+        {
+            this.transition = transition;
+            this.init_x = prev_x;
+
+            col_dim = transition.GetLength(1);
+            row_dim = prev_x.GetLength(0);
+
+            if (col_dim != row_dim)
+                throw new ArgumentException("Argument dimensions not valid for a dot product");
+
+            result_row_dim = transition.GetLength(0);
+            result_col_dim = prev_x.GetLength(1);
+        }
+
+        public double[][,] CalculateXs(int iterations)
+        {
+            double[][,] xs = new double[iterations][,];
+            double[,] x = init_x;
+
+            xs[0] = x;
+
+            for (int i = 1; i < iterations; i++)
+            {
+                x = Dot(x);
+                xs[i] = x;
+            }
+
+            return xs;
+        }
+
+        private double[,] Dot(double[,] x)
+        {
+            double[,] next_x = new double[result_row_dim,result_col_dim];
+
+            for(int k = 0; k < result_col_dim; k++)
+            {
+                for (int i = 0; i < result_row_dim; i++)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < row_dim; j++)
+                    {
+                        sum += transition[i, j] * x[j, k];
+                    }
+
+                    next_x[i, k] = sum;
+                }
+            }
+
+            return next_x;
+        }
+    }
+}
--- Markov Chain/Program.cs
+++ Markov Chain/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Markov_Calculator;
 
 namespace Markov_Chain
 {
@@ -10,6 +11,15 @@ namespace Markov_Chain
     {
         static void Main(string[] args)
         {
+            double[,] trans = new double[,] { { 0.8, 0.4 }, { 0.2, 0.6}  };
+            double[,] init_x = new double[,] { { 0.5}, { 0.5} };
+
+            Calculator calc = new Calculator(trans, init_x);
+
+            double[][,] xs = calc.CalculateXs(50);
+
+            Console.WriteLine(xs);
+            Console.Read();
         }
     }
 }
